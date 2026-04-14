3ca7a0bbfc637dac18c84fe6900c0ef0a8ffb3fa
aabsolutions <angelbs.1985@gmail.com>
2026-04-13 23:13:30 -0500

    Implement crud materias y funcionalidad de registro de carga horaria

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'https://school-admin-backend-ueee.onrender.com/api',
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://school-admin-backend-ueee.onrender.com/api',
+  //apiUrl: 'http://localhost:3000/api',
 };
 
 /*
