9e9f2879fbfac2af33a2fa29311b0fb3fb933e0c
aabsolutions <angelbs.1985@gmail.com>
2026-06-12 00:52:08 -0500

    Auto-generated commit

---
+++ chunk-MAGP7OVF.js
@@ -0,0 +1,1 @@
+import"./chunk-WWX6BADO.js";var e=[{path:"",redirectTo:"docentes",pathMatch:"full"},{path:"docentes",loadComponent:()=>import("./chunk-W7BVI35M.js").then(t=>t.DocentesDocumentalComponent)},{path:"estudiantes",loadComponent:()=>import("./chunk-23NAPLXB.js").then(t=>t.EstudiantesDocumentalComponent)}];export{e as GESTION_DOCUMENTAL_ROUTE};
