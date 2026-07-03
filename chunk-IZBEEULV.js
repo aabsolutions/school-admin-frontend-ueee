f97929031c77f9b45e68ee4645d46628125be9ac
aabsolutions <angelbs.1985@gmail.com>
2026-07-02 23:59:17 -0500

    Auto-generated commit

--- chunk-IZBEEULV.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-WWX6BADO.js";var t=[{path:"",loadComponent:()=>import("./chunk-SKSR53WO.js").then(o=>o.AllDeceComponent)}];export{t as DECE_ROUTE};
--- chunk-VL4YMCF5.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-WWX6BADO.js";var e=[{path:"",redirectTo:"docentes",pathMatch:"full"},{path:"docentes",loadComponent:()=>import("./chunk-DLASALI6.js").then(t=>t.DocentesDocumentalComponent)},{path:"estudiantes",loadComponent:()=>import("./chunk-BHJRJ4NN.js").then(t=>t.EstudiantesDocumentalComponent)}];export{e as GESTION_DOCUMENTAL_ROUTE};
